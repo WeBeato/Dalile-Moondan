163f00057486e3c5cbcc8cf92cab2bcd0ff499a0
webeato <webeato.ad@gmail.com>
2026-02-19 17:24:29 +0330

    create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css'
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img src="../../public/images/logo.png" alt="لوگو" className="logo" />
+    </div>
+  );
+}
