f75368d1eb6c591170e0f0af64b087a783c06aac
webeato <webeato.ad@gmail.com>
2026-02-20 16:33:57 +0330

    show messages from firestore

--- src/components/CardMessage.jsx
+++ src/components/CardMessage.jsx
@@ -1,9 +1,21 @@
+import { useFirestore } from "../hooks/useFirestore";
 import "./CardMessage.css";
 
 export default function CardMessage() {
+  const { docs: messages } = useFirestore("messages");
+
   return (
     <div className="card-message">
-      <h2>پیام امید</h2>
+      {messages &&
+        messages.map((message) => (
+          <div className="message">
+            <p className="message__text">{message.textMessage}</p>
+            <p className="message__author">{message.author}</p>
+          </div>
+        ))}
+        <button className="card-message__btn">
+          یکی دیگه
+        </button>
     </div>
   );
 }
