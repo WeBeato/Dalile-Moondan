b0f45362581925400f5a3e6636c7dbbae0c4e341
webeato <webeato.ad@gmail.com>
2026-02-22 00:09:00 +0330

    fix some bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import "./Header.css";
+import headerLogo from "../../public/imageslogo.png";
 import { Link } from "react-router";
 
 export default function Header() {
   return (
     <div className="header">
       <Link to="/">
-        <img src="../../public/images/logo.png" alt="لوگو" className="header__logo" />
+        <img src={headerLogo} alt="لوگو" className="header__logo" />
       </Link>
       <Link to="/saved-messages" className="header__btn">
         پیام‌های ذخیره شده
